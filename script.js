efabeace666692a255aedaf440ccf32c6b659773
Abylkairkhan <a_sarsetaev@kbtu.kz>
2024-02-15 04:12:13 +0600

    Uma Fix

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-function nextPage() {
-    window.location.href = "yes.html";
-}
+initWidth = 5
+initHeight = 4
+$(document).ready(function() {
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $('#reset');
+    
+    envelope.click(function() {
+        open();
+    });
+    btn_open.click(function() {
+        open();
+    });
+    btn_reset.click(function() {
+        close();
+    });
+    
+    function open() {
+        envelope.addClass('open')
+            .removeClass('close');
+    }
+    function close() {
+        envelope.addClass('close')
+            .removeClass('open');
+    
+    function openWithDelay() {
+        open();
+        setTimeout(function() {
+            close();
+            showAlert();
+        }, 5000); 
+    }
+    }
+})
+document.getElementById("reset").addEventListener("click",()=>{
+    initWidth += 3;
+    initHeight += 2;
+    document.getElementById("open").style.width = `${initWidth}rem`;
+    document.getElementById("open").style.height = `${initHeight}rem`;
 
-function moveButton() {
-    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth);
-    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight);
-    document.getElementById('noButton').style.left = `${x}px`;
-    document.getElementById('noButton').style.top = `${y}px`;
-}
+})
+document.getElementById('open').addEventListener('click', () => {
+
+    alert("Я так рад что ты выбрала Да) \nНемного опоздал с валентинкой, хотел сделать как можно необычнее\nС тобой весело и забавно♡\nПосле встречи с тобой я сохранил 119 автобус и ДоДо в отдельном месте своего сердца♡\nНадеюсь ты будешь меньше игнорить и чаще отвечать˚ʚ♡ɞ˚\nНо в любом случае я буду ждать, любить и бесить тебя вечно♡\nНадеюсь мы сможем еще раз увидеться)")
+})
